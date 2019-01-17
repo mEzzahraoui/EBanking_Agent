b586e139f83c73331b2aa15700be0937c4b95fad
EZZAHRAOUI Meriame <35901535+mEzzahraoui@users.noreply.github.com>
2019-01-17 18:53:35 +0000

    Update WSConfigClient.java

--- src/main/java/com/meriame/WSConfigClient.java
+++ src/main/java/com/meriame/WSConfigClient.java
@@ -17,19 +17,11 @@ public class WSConfigClient {
 		return marshaller;
 	}
 	
-	@Bean
-	public AdminClient articleClient(Jaxb2Marshaller marshaller) {
-		AdminClient client = new AdminClient();
-		client.setDefaultUri("https://g-bank.herokuapp.com:8044/services/AdminService?wsdl");
-		client.setMarshaller(marshaller);
-		client.setUnmarshaller(marshaller);
-		return client;
-	}
 	
 	@Bean
 	public AgentService agentService(Jaxb2Marshaller marshaller) {
 		AgentService agentServ=new AgentService();
-		agentServ.setDefaultUri("https://g-bank.herokuapp.com:8055/services/AgentService?wsdl");
+		agentServ.setDefaultUri("https://g-bank.herokuapp.com/services/AgentService?wsdl");
 		agentServ.setMarshaller(marshaller);
 		agentServ.setUnmarshaller(marshaller);
 		return agentServ;
