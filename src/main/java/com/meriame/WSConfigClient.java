a44716f211a67a859d3f19763a691e719250a7d9
Imane <imaneherraz16@gmail.com>
2019-01-18 14:02:28 +0100

    consommation from heroku

--- src/main/java/com/meriame/WSConfigClient.java
+++ src/main/java/com/meriame/WSConfigClient.java
@@ -29,7 +29,7 @@ public class WSConfigClient {
 	@Bean
 	public AgentService agentService(Jaxb2Marshaller marshaller) {
 		AgentService agentServ=new AgentService();
-		agentServ.setDefaultUri("http://127.0.0.1:8055/services/AgentService?wsdl");
+		agentServ.setDefaultUri("https://g-bank.herokuapp.com/services/AgentService?wsdl");
 		agentServ.setMarshaller(marshaller);
 		agentServ.setUnmarshaller(marshaller);
 		return agentServ;
